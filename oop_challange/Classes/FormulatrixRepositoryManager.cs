5d04d0e50a2ade47baf26f3ed14db073d6758a35
Anton <heni.kurniyanto@kuncie.com>
2023-12-16 10:54:10 +0700

    feat(tempStorage): saving data to temporary storage/memory

---
+++ oop_challange/Classes/FormulatrixRepositoryManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace oop_challange
+{
+	public class FormulatrixRepositoryManager
+	{
+        private Dictionary<string, Tuple<string, int>> dataStore;
+        private bool isInitialized = false;
+
+        // Initialize the repository for use
+        public void Initialize()
+        {
+            if (!isInitialized)
+            {
+                dataStore = new Dictionary<string, Tuple<string, int>>();
+                isInitialized = true;
+            }
+            else
+            {
+                Console.WriteLine("Repository has already been initialized.");
+            }
+        }
+
+        // Store an item to the repository
+        public void Register(string itemName, string itemContent, int itemType)
+        {
+            if (!isInitialized)
+            {
+                Console.WriteLine("Repository has not been initialized. Call Initialize method first.");
+                return;
+            }
+
+            if (dataStore.ContainsKey(itemName))
+            {
+                Console.WriteLine($"Item with name '{itemName}' already exists. Use a different name.");
+                return;
+            }
+
+            // Validate itemContent based on the given itemType
+            
+            // Store the item in the dataStore
+            dataStore[itemName] = new Tuple<string, int>(itemContent, itemType);
+            Console.WriteLine($"Item '{itemName}' registered successfully.");
+        }
+
+        // Retrieve an item from the repository
+        public string Retrieve(string itemName)
+        {
+            if (!isInitialized)
+            {
+                Console.WriteLine("Repository has not been initialized. Call Initialize method first.");
+                return null;
+            }
+
+            if (dataStore.ContainsKey(itemName))
+            {
+                return dataStore[itemName].Item1;
+            }
+            else
+            {
+                Console.WriteLine($"Item with name '{itemName}' not found.");
+                return null;
+            }
+        }
+
+        // Retrieve the type of the item (JSON or XML)
+        public int GetType(string itemName)
+        {
+            if (!isInitialized)
+            {
+                Console.WriteLine("Repository has not been initialized. Call Initialize method first.");
+                return -1;
+            }
+
+            if (dataStore.ContainsKey(itemName))
+            {
+                return dataStore[itemName].Item2;
+            }
+            else
+            {
+                Console.WriteLine($"Item with name '{itemName}' not found.");
+                return -1;
+            }
+        }
+
+        // Remove an item from the repository
+        public void Deregister(string itemName)
+        {
+            if (!isInitialized)
+            {
+                Console.WriteLine("Repository has not been initialized. Call Initialize method first.");
+                return;
+            }
+
+            if (dataStore.ContainsKey(itemName))
+            {
+                dataStore.Remove(itemName);
+                Console.WriteLine($"Item '{itemName}' deregistered successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Item with name '{itemName}' not found.");
+            }
+        }
+    }
+}
+
---
+++ oop_challange/Oop_App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oop_challange
+{
+    class Oop_App
+    {
+        static void Main()
+        {
+            FormulatrixRepositoryManager repositoryManager = new FormulatrixRepositoryManager();
+            repositoryManager.Initialize();
+
+            // Register items
+            repositoryManager.Register("json1", "{ 'name': 'Anton', 'age': 30 }", 1);
+            repositoryManager.Register("xml1", "<person><name>Atnlie</name><age>21</age></person>", 2);
+            repositoryManager.Register("json2", "{ 'name': 'Budi', 'age': 35 }", 1);
+            repositoryManager.Register("xml2", "<person><name>Budilie</name><age>41</age></person>", 2);
+
+            // Retrieve item
+            Console.WriteLine("Retrieved JSON item: " + repositoryManager.Retrieve("json1"));
+            Console.WriteLine("Retrieved XML item: " + repositoryManager.Retrieve("xml1"));
+
+            Console.WriteLine("Retrieved JSON item: " + repositoryManager.Retrieve("json2"));
+            Console.WriteLine("Retrieved XML item: " + repositoryManager.Retrieve("xmlI2"));
+
+            // Retrieve item type
+            Console.WriteLine("Type of jsonItem: " + repositoryManager.GetType("json1"));
+            Console.WriteLine("Type of xmlItem: " + repositoryManager.GetType("xml1"));
+
+            // Deregister an item
+            repositoryManager.Deregister("json1");
+            repositoryManager.Deregister("json1");
+
+            Console.WriteLine("Retrieved JSON json1: " + repositoryManager.Retrieve("json1"));
+            Console.WriteLine("Retrieved Json json2: " + repositoryManager.Retrieve("json2"));
+        }
+    }
+}
